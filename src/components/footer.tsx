2b6d806bc59868ae0ca56ff6bc5cc41384342158
Mohsin Ghani <Mohsinghani.777@gmail.com>
2023-07-07 12:52:10 +0500

    minor fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,11 +9,11 @@ const CommonFooter = () => {
         <div className="flex flex-col-reverse w-full items-center justify-between sm:flex-row  mt-[30px]  mb-[30px]">
           <div className="flex flex-col-reverse gap-[42px]  sx:flex-col sm:flex-row">
             <div className="items-end flex flex-col leading-9 text-[20px]">
-              <p>تعارف</p>
-              <p>رابتا</p>
-              <p>مالی امداد</p>
-              <p>رضاکار</p>
-              <p> پرائیویسی پالیسی </p>
+              <a className="cursor-pointer">تعارف</a>
+              <a className="cursor-pointer">رابتا</a>
+              <a className="cursor-pointer">مالی امداد</a>
+              <a className="cursor-pointer">رضاکار</a>
+              <a className="cursor-pointer"> پرائیویسی پالیسی </a>
             </div>
             <div className="leading-9 ">
               <div className="flex mt-[30px] sm:mt-[0px] w-full ">
@@ -45,8 +45,10 @@ const CommonFooter = () => {
                 <p className="flex justify-end">Download shaoor app</p>
                 <div className="flex justify-end">
                   <img
-                    className="w-[50%] h-10 bg-none items-center flex mt-4"
-                    src="/assets/images/app-store.jpg"
+                    className="w-full h-12 bg-none cursor-pointer
+                     items-center flex mt-4"
+                    src="/assets/images/dono.png
+                    "
                   />
                 </div>
               </div>
@@ -67,14 +69,14 @@ const CommonFooter = () => {
             </div>
             <div className="flex  flex-row-reverse items-center ">
               <p className="ml-4 font-bold  text-xl">فون</p>
-              <p className="text-[#a4c6eb] font-bold text-base font-sans-serif">
+              <p className="text-[#a4c6eb] font-bold text-base font-sans-serif cursor-pointer">
                 {" "}
                 +92 -333 -435673
               </p>
             </div>
             <div className="flex  flex-row-reverse items-center">
               <p className="ml-4 font-bold  text-xl">ای میل</p>
-              <p className="text-[#a4c6eb] font-bold text-base font-sans-serif">
+              <p className="text-[#a4c6eb] font-bold text-base font-sans-serif cursor-pointer">
                 shaoorpk1@gmail.com
               </p>
             </div>
@@ -88,7 +90,7 @@ const CommonFooter = () => {
             <div>
               {" "}
               <img
-                className="w-full h-36 mt-6"
+                className="w-full h-36 mt-6 cursor-pointer"
                 src="/assets/images/HILmr.png"
               />
             </div>
